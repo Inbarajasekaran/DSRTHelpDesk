c3940fac64fee64d79e268c9166ab8ebedb8d118
Inbarajasekaran <inbarajasekaran0011@gmail.com>
2021-05-06 12:38:32 +0530

    commit data binding on query list with default issue status

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -20,14 +20,14 @@ export class ApiServiceService {
       userid: undefined,
       issueid: 1,
       issue: "ISSUE NAME 1",
-      status: undefined,
+      status: this.ISSUE_STATUS[0]['STATUS'],
       fileInfo: ["E:\test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for u"]
     },
     {
       userid: undefined,
       issueid: 2,
       issue: "ISSUE NAME 2",
-      status: undefined,
+      status: this.ISSUE_STATUS[0]['STATUS'],
       fileInfo: ["E:\test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for upload file in DSRT HELP DESK test file for u"]
     },
   ]
